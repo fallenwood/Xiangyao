4d57cb48484fcf55854f63057c29ba86b5ee361b
Fallenwood <fallenwood.y@outlook.com>
2024-08-08 22:37:49 +0800

    Revert "fix"
    This reverts commit c5c833d74e6cbfd6b53492270adaa494452b9973.

--- make-aot.sh
+++ make-aot.sh
@@ -2,5 +2,5 @@
 
 head -n 1 src/Xiangyao/Xiangyao.csproj | cat > src/Xiangyao/Xiangyao.csproj.1
 echo "<PropertyGroup><PublishAot>true</PublishAot></PropertyGroup>" >> src/Xiangyao/Xiangyao.csproj.1
-tail -n +2 src/Xiangyao/Xiangyao.csproj | cat >> src/Xiangyao/Xiangyao.csproj.1
+tail -n +1 src/Xiangyao/Xiangyao.csproj | cat >> src/Xiangyao/Xiangyao.csproj.1
 mv src/Xiangyao/Xiangyao.csproj.1 src/Xiangyao/Xiangyao.csproj
